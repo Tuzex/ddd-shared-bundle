5d7b104f1dbc6d04b2d8d1cd70328ae5e70660a9
Edo Barnas <iam@edobarnas.com>
2022-02-26 14:31:49 +0100

    Implement `Symfony` bundle

---
+++ src/DddSharedBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tuzex\Bundle\Ddd\Shared;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\HttpKernel\Bundle\BundleInterface;
+
+final class DddSharedBundle extends Bundle implements BundleInterface
+{
+}
